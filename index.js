b081cf2882c1fdcf16ac9e5106d50c5893081ff4
John Hennig <142534166+jongasss@users.noreply.github.com>
2023-11-16 18:08:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+// console.log("Meu nome é John")
+
+// prompt("Digite seu nome")
+
+
+// const nome = John
+
+// const sobrenome = Hennig
+
+// const idade = 16
+
+// const empregado = false
+
+// console.log("Olá, meu nome é", nome, "e meu sobrenome é", sobrenome, "tenho", idade, "anos. e estou desempregado", empregado)
+
+// console.log("Olá, meu nome é"+ nome+ "e meu sobrenome é"+ sobrenome+ "tenho"+ idade+ "anos. e estou desempregado"+ empregado)
+
+// const numero = 123
+// const numeroComoString = numero.toString()
+// console.log(typeof numeroComoString);// Saída:"string"
+
+// const stringNumerica = "456";
+// const stringConvertidaParaNumero = Number(stringNumerica);
+// console.log(typeof      stringConvertidaParaNumero);    // saída "number"
+
+let nome = null;
+let cidade = null;
+
+console.log(typeof nome, typeof cidade);
+
+nome = prompt("digite seu nome");
+cidade = prompt("digite sua cidade");
+
+console.log(typeof nome, typeof cidade);
+
+console.log("Olá, meu nome é", nome, "e eu moro em", cidade, ".");  
